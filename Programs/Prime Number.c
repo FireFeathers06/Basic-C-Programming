5fc4cb89ecc44f5a627baf9e164d4c0c347be374
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-10-07 13:47:15 -0700

    upadates prime number to only check to square root of number to check and adds a check to make sure number > 1

--- Programs/Prime Number.c
+++ Programs/Prime Number.c
@@ -1,19 +1,28 @@
-#include<stdio.h>  
-int main(){    
-int n,i,m=0,flag=0;    
-printf("Enter the number to check prime:");    
-scanf("%d",&n);    
-m=n/2;    
-for(i=2;i<=m;i++)    
-{    
-if(n%i==0)    
-{    
-printf("Number is not prime");    
-flag=1;    
-break;    
-}    
-}    
-if(flag==0)    
-printf("Number is prime");     
-return 0;  
- }    
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  int n, i, m, flag = 0;
+  printf("Enter the number to check prime: ");
+  scanf("%d", &n);
+  if (n < 2)
+  {
+    printf("Please enter a number larger than 1\n");
+    flag = 1;
+  }
+  // only have to go to square root of the number, not n/2
+  m = sqrt(n);
+  for (i = 2; i <= m; i++)
+  {
+    if (n % i == 0)
+    {
+      printf("Number is not prime\n");
+      flag = 1;
+      break;
+    }
+  }
+  if (flag == 0)
+    printf("Number is prime\n");
+  return 0;
+}
