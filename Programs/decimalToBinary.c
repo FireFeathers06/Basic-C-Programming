c8d9344f89251f90f0dca9e3692e8d8adcd120dd
Aman Kumar Jagdev <34389172+amanjagdev@users.noreply.github.com>
2019-10-08 11:29:20 +0530

    Update decimalToBinary.c

--- Programs/decimalToBinary.c
+++ Programs/decimalToBinary.c
@@ -2,19 +2,23 @@
 #include<stdlib.h>  
 
 int main(){  
-int a[10],n,i;    
-system ("cls");  
-printf("Enter the number to convert: ");    
-scanf("%d",&n);    
-for(i=0;n>0;i++)    
-{    
-a[i]=n%2;    
-n=n/2;    
-}    
-printf("\nBinary of Given Number is=");    
-for(i=i-1;i>=0;i--)    
-{    
-printf("%d",a[i]);    
-}    
+    //Declared Variables
+    int a[10],n,i;  
+
+    //Entering the number
+    printf("Enter the number to convert: ");    
+    scanf("%d",&n);    
+    
+    for(i=0;n>0;i++){    
+        a[i]=n%2;    
+        n=n/2;    
+    } 
+
+    //Printing the Number
+    printf("\nBinary of Given Number is=");    
+    for(i=i-1;i>=0;i--){    
+        printf("%d",a[i]);    
+    }  
+      
 return 0;  
 }  
