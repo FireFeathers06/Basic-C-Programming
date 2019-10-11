12edae87d9d501355eb88bc4766fa8f49fcbc282
Priyansi <priyanhiman@gmail.com>
2019-10-11 10:46:21 +0530

    Update armstrongNumber.c
    Updated the code to accommodate all Armstrong numbers not just 3 digit ones.

--- Programs/armstrongNumber.c
+++ Programs/armstrongNumber.c
@@ -1,21 +1,28 @@
 /*
 
-Armstrong number is a number that is equal to the sum of cubes of its digits. For example 0, 1, 153, 370, 371 and 407 are the Armstrong numbers.
+Armstrong number is a number that is equal to the sum of the nth power of the digits where n is the number of digits. For example 0, 1, 153, 370, 371, 407 and 1634 are the Armstrong numbers.
 
 */
 
 
 #include<stdio.h>  
+#include<math.h>
  int main()    
 {    
-int n,r,sum=0,temp;    
+int n,r,sum=0,temp,d=0; // d is the number of digits   
 printf("enter the number=");    
 scanf("%d",&n);    
-temp=n;    
+temp=n;
+while(temp>0)
+{
+ d++;
+ temp=temp/10;
+}
+temp=n;
 while(n>0)    
 {    
 r=n%10;    
-sum=sum+(r*r*r);    
+sum=sum+(pow(r,d)); //using pow() which is a function of math.h
 n=n/10;    
 }    
 if(temp==sum)    
