95760098688b6989203ee8cc31c3defa7453df7e
arnavbansal2764 <70932975+arnavbansal2764@users.noreply.github.com>
2020-10-01 09:02:41 +0530

    Update DecToBinary.c

--- Programs/DecToBinary.c
+++ Programs/DecToBinary.c
@@ -1,23 +1,47 @@
-    #include<stdio.h>
-    #include<stdlib.h>
+//     #include<stdio.h>
+//     #include<stdlib.h>
 
-    int main(){
+//     int main(){
 
-    int a[10],n,i;
-    system ("cls");
-    printf("Enter the number to convert: ");
-    scanf("%d",&n);
+//     int a[10],n,i;
+//     system ("cls");
+//     printf("Enter the number to convert: ");
+//     scanf("%d",&n);
 
-    for(i=0;n>0;i++)
-      {
-       a[i]=n%2;
-       n=n/2;
-      }
+//     for(i=0;n>0;i++)
+//       {
+//        a[i]=n%2;
+//        n=n/2;
+//       }
 
-    printf("\nBinary of Given Number is=");
-    for(i=i-1;i>=0;i--)
-     {
-       printf("%d",a[i]);
-     }
-    return 0;
-    }
+//     printf("\nBinary of Given Number is=");
+//     for(i=i-1;i>=0;i--)
+//      {
+//        printf("%d",a[i]);
+//      }
+//     return 0;
+//     }
+#include <stdio.h>
+
+int main() {
+int n;
+printf ("Enter the number to be coverted in binary : ");
+scanf ("%d", &n);
+int rem;
+int a[15];
+int i = 0;
+
+while(n)
+{
+    rem = n%2 ;
+    n = n/2;
+    a[i]= rem;
+    i++;
+}
+for (int j = i - 1; j >=0; j--)
+{
+    printf("%d", a[j]);
+}
+
+return 0;
+}
