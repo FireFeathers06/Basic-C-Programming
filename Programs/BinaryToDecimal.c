e89121bdaf84b00e356a4149b122811a88784a72
bharti meena <60185429+bhartimeena@users.noreply.github.com>
2020-10-03 04:02:42 -0700

    Update BinaryToDecimal.c

--- Programs/BinaryToDecimal.c
+++ Programs/BinaryToDecimal.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <math.h>
-int convertBinaryToDecimal(long long n);
+int convertBinaryToDecimal(long n);
 int main()
 {
-    long long n;
+   long n;
     printf("Enter a binary number: ");
-    scanf("%lld", &n);
-    printf("%lld in binary = %d in decimal", n, convertBinaryToDecimal(n));
+    scanf("%ld", &n);
+    printf("Equivalent  decimal number is: %d in decimal", n, convertBinaryToDecimal(n));
     return 0;
 }
-int convertBinaryToDecimal(long long n)
+int convertBinaryToDecimal(long n)
 {
     int decimalNumber = 0, i = 0, remainder;
     while (n!=0)
     {
         remainder = n%10;
-        n /= 10;
-        decimalNumber += remainder*pow(2,i);
-        ++i;
+        binarynum = n/10;
+        decimalNumber = decimalnum+remainder*pow(2,i);
+        i++;
     }
     return decimalNumber;
 }
